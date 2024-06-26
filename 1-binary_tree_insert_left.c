62e73ac8fd9eeaae963e07ba74c2543dd133e0a6
vokske <kevinmuigai2@gmail.com>
2024-06-26 17:53:48 +0300

    Insert a node as left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
                 return (NULL);
 
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 		return (NULL);
